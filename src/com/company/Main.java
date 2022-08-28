78ec9009b06e3c5ed34cacb0438d79a193ff2f08
Aimal Mansory <aimalmansory21@gmail.com>
2022-08-28 15:21:39 -0700

    Solar system ready to be deployed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         // Write your code here
-        
+        solarSystem inception = new solarSystem();
+        inception.sun=1;
+        inception.Moon=4;
+        inception.planets=2;
+        System.out.println("The basic structure is ready ");
+
     }
 }
