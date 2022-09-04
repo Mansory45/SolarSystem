a34be2e87bea52325f126de6a1c581d418052325
Aimal Mansory <aimalmansory21@gmail.com>
2022-09-03 17:28:05 -0700

    The second branch has been added in the current project

---
+++ src/com/company/GitBranchTest.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class GitBranchTest {
+    public static void main(String[] args) {
+        System.out.println("Aimal Mansory is the greats Software Engineer in Test");
+    }
+}
